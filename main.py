2856795baac883b60c4f2758ea13ac9941417ffc
Leology0 <leology42@gmail.com>
2023-09-20 13:19:44 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,65 +1,74 @@
-# survey_file = open("survey-results.txt")
+def survey_search():
+    survey_file = open("survey-results.txt")
 
-# survey_list = []
-# for line in survey_file:
-#     line = line.strip()
-#     survey_list.append(line)
+    survey_list = []
+    for line in survey_file:
+        line = line.strip()
+        survey_list.append(line)
 
-# survey_file.close()
+    survey_file.close()
 
-# yes_count = 0
-# no_count = 0
-# maybe_count = 0
-# for i in range(len(survey_list)):
-#     if survey_list[i] == "Yes":
-#         yes_count += 1
-#     elif survey_list[i] == "No":
-#         no_count += 1
-#     elif survey_list[i] == "Maybe":
-#         maybe_count += 1
+    yes_count = 0
+    no_count = 0
+    maybe_count = 0
+    for i in range(len(survey_list)):
+        if survey_list[i] == "Yes":
+            yes_count += 1
+        elif survey_list[i] == "No":
+            no_count += 1
+        elif survey_list[i] == "Maybe":
+            maybe_count += 1
 
-# print(str(yes_count) + " " + str(no_count) + " " + str(maybe_count))
+    print("Yes " + str(yes_count) + " No " + str(no_count) + " Maybe " + str(maybe_count))
 
-age_file = open("age-data.txt")
+def age_search():
+    age_file = open("age-data.txt")
 
-age_list = []
-for line in age_file:
-    line = line.strip()
-    age_list.append(int(line))
+    age_list = []
+    for line in age_file:
+        line = line.strip()
+        age_list.append(int(line))
 
-age_file.close()
+    age_file.close()
 
-under_18_count = 0
-between_18_and_35_count = 0
-between_36_and_65_count = 0
-above_65_count = 0
-for i in range(len(age_list)):
-    if age_list[i] < 18:
-        under_18_count += 1
-    elif age_list[i] <= 35:
-        between_18_and_35_count += 1
-    elif age_list[i] <= 65:
-        between_36_and_65_count += 1
-    else:
-        above_65_count += 1
+    under_18_count = 0
+    between_18_and_35_count = 0
+    between_36_and_65_count = 0
+    above_65_count = 0
+    for i in range(len(age_list)):
+        if age_list[i] < 18:
+            under_18_count += 1
+        elif age_list[i] <= 35:
+            between_18_and_35_count += 1
+        elif age_list[i] <= 65:
+            between_36_and_65_count += 1
+        else:
+            above_65_count += 1
 
-print("Under 18 (" + str(under_18_count) + "), 18 to 35 (" + str(between_18_and_35_count) + "), 36 to 65 (" + str(between_36_and_65_count) + "), Above 65 (" + str(above_65_count) + ")")
+    print("Under 18 (" + str(under_18_count) + "), 18 to 35 (" + str(between_18_and_35_count) + "), 36 to 65 (" + str(between_36_and_65_count) + "), Above 65 (" + str(above_65_count) + ")")
 
-# number_file = open("number-data.txt")
+def number_search():
+    number_file = open("number-data.txt")
 
-# number_list = []
-# for line in number_file:
-#     line = line.strip()
-#     number_list.append(int(line))
+    number_list = []
+    for line in number_file:
+        line = line.strip()
+        number_list.append(int(line))
 
-# number_file.close()
+    number_file.close()
 
-# even_count = 0
-# odd_count = 0
-# for i in range(len(number_list)):
-#     if number_list[i]  % 2 == 0:
-#         even_count += 1
-#     else:
-#         odd_count += 1
+    even_count = 0
+    odd_count = 0
+    for i in range(len(number_list)):
+        if number_list[i]  % 2 == 0:
+            even_count += 1
+        else:
+            odd_count += 1
 
-# print("Even (" + str(even_count) + "), Odd (" + str(odd_count) + ")")
+    print("Even (" + str(even_count) + "), Odd (" + str(odd_count) + ")")
+
+# survey_search()
+
+# age_search()
+
+# number_search()
